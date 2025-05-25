47f420ec609e058007a25f30ef5c5814634d49af
Anjila Tripathi <anjila.tripathi@gmail.com>
2025-05-25 09:49:05 +0545

    Test Data for Login

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,2 +1,21 @@
-public class test {
+import base.Base;
+import org.testng.annotations.Test;
+import pages.Login;
+
+public class LoginTest extends Base
+{
+    @Test
+    public void testInvalidLogin()
+    {
+        Login login = new Login(driver,wait);
+        login.inValidLogin("locked_out_user","secret_sauce");
+
+    }
+    @Test
+    public void testValidLogin()
+{
+    Login login = new Login(driver,wait);
+    login.validLogin("standard_user","secret_sauce");
+}
+
 }
